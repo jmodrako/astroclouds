830f86c9b72a22a5b229eb2f18b893e851b54547
Jacek Modrakowski <jacek.beny@gmail.com>
2023-03-07 16:39:52 +0100

    Add main py

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+import random
+import rp2
+import network
+import ubinascii
+import urequests as requests
+import time
+import socket
+import re
+from machine import I2C, Pin
+
+"""Your main code goes here!"""
+
+print("Accept the pampering! ~ Senko")
+
+# Define blinking function for onboard LED to indicate error codes    
+def blink_onboard_led(num_blinks):
+    led = machine.Pin('LED', machine.Pin.OUT)
+    for i in range(num_blinks):
+        led.on()
+        time.sleep(.2)
+        led.off()
+        time.sleep(.2)
+
+blink_onboard_led(10)
+
